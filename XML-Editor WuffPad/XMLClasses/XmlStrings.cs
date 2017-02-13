01e9d5f7855769b188f9b68d2dacc118e72af576
Olfi01 <flom@meyer-alpers.de>
2017-02-13 15:29:44 +0100

    Made it easier to change target framework

--- XML-Editor WuffPad/XMLClasses/XmlStrings.cs
+++ XML-Editor WuffPad/XMLClasses/XmlStrings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Xml.Serialization;
 
 namespace XML_Editor_WuffPad.XMLClasses
